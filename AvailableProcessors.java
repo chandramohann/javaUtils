1d7cb1c010198e2a7c8bd0e307f15c24099df7a9
chandramohan <chandramohan.nagaraj@gmail.com>
2020-02-25 13:45:59 +0100

    AvailableProcessors
    CPU checker

---
+++ AvailableProcessors.java
@@ -0,0 +1,6 @@
+public class AvailableProcessors {
+public static void main(String[] args) {
+      //get available processors
+      System.out.println(""+Runtime.getRuntime().availableProcessors());
+   }
+}
